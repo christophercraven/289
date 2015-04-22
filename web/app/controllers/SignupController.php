4d369a9c4222faba094b6ec3845b40bf796465f1
unknown <BAL11714@BAL11714.balsam.asheville.cc.nc.us>
2015-04-22 17:37:09 -0400

    added captcha

--- web/app/controllers/SignupController.php
+++ web/app/controllers/SignupController.php
@@ -14,62 +14,72 @@ class SignupController extends ControllerBase
 	}
 
 	public function registerAction()
-	{
-        // Include password settings
-        require "../app/library/Generator.php";
-        
-        // Make a temporary random password
-        $generated = generateRandom();
-        $temporary = $generated;
-        // Password salt and hash with 2 methods
-        $secret = $option["secret"];
-        $generated = hashIt($generated, $secret);
-        $generated = $this->security->hash($generated . $secret); 
-        
-        // Get $_POST variables
-		$postPost = $this->request->getPost();
-        $postPost["password"] = $generated;
+	{	
+		if ( isset($_POST['g-recaptcha-response']) ) {
+			$secret = "6LfCxgUTAAAAAM6kObz3WB_OXTl2wf-z7HTM0P20";
+			$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+			$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+			if ($resp->isSuccess()):
 
-        // Intantiate user database
-		$user = new Users();
-        
-        // Prepare fields, preventing SQL injection is handled automatically
-        $user->first_usr = $postPost["name"];
-        $user->email_usr = $postPost["email"];
-        $user->pw_usr    = $postPost["password"];
-        
-		// Store and check for errors
-		echo '<div class="container">';
-        try {
-            $success = $user->create();
-            if ($success) {
-                echo "<h1>Congratulations, " . ucfirst($postPost['name']) . "</h1>";
-                echo "<p>New account added! </p>";
-				echo "<p>Your temporary password is <code>".$temporary."</code></p>";
-				echo "<p>Passwords are case sensitive.</p>";
-				echo "<p>Please login and change the password for increased security.</p>";
+				// Include password settings
+				require "../app/library/Generator.php";
+
+				// Make a temporary random password
+				$generated = generateRandom();
+				$temporary = $generated;
+				// Password salt and hash with 2 methods
+				$secret = $option["secret"];
+				$generated = hashIt($generated, $secret);
+				$generated = $this->security->hash($generated . $secret); 
 				
-            } else {
-				echo "Sorry, the following problems occurred: <ul>";
-				foreach ($user->getMessages() as $message) {
-					echo "<li>", $message->getMessage(), "</li>";
-				}
-				echo "</ul>";
-			}
-        }
-        catch(Exception $e) {
-			
-            $errorMessage = $e->getMessage();
-			if ( strpos( $errorMessage, '1062' )) {
-                echo "<span class=\"alert\">Account already exists: </span>" . $postPost["email"]; 
-                
-            } else {
-				echo 'Message: ' .$e->getMessage();
-			}
-        }
-		
+				// Get $_POST variables
+				$postPost = $this->request->getPost();
+				$postPost["password"] = $generated;
 
-		echo "</div>";
-		//$this->view->disable();
+				// Intantiate user database
+				$user = new Users();
+				
+				// Prepare fields, preventing SQL injection is handled automatically
+				$user->first_usr = $postPost["name"];
+				$user->email_usr = $postPost["email"];
+				$user->pw_usr    = $postPost["password"];
+				
+				// Store and check for errors
+				echo '<div class="container">';
+				try {
+					$success = $user->create();
+					if ($success) {
+						echo "<h1>Congratulations, " . ucfirst($postPost['name']) . "</h1>";
+						echo "<p>New account added! </p>";
+						echo "<p>Your temporary password is <code>".$temporary."</code></p>";
+						echo "<p>Passwords are case sensitive.</p>";
+						echo "<p>Please login and change the password for increased security.</p>";
+						
+					} else {
+						echo "Sorry, the following problems occurred: <ul>";
+						foreach ($user->getMessages() as $message) {
+							echo "<li>", $message->getMessage(), "</li>";
+						}
+						echo "</ul>";
+					}
+				}
+				catch(Exception $e) {
+					
+					$errorMessage = $e->getMessage();
+					if ( strpos( $errorMessage, '1062' )) {
+						echo "<span class=\"alert\">Account already exists: </span>" . $postPost["email"]; 
+						
+					} else {
+						echo 'Message: ' .$e->getMessage();
+					}
+				}
+				
+
+				echo "</div>";
+				//$this->view->disable();
+			else:
+				$this->flash->error('Captcha not completed. Please go back and try again. ');
+			endif;
+		}
 	}
 }
