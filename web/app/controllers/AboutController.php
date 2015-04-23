2fe84dd89bdb1821618b0acd16cd7cbf5af71f73
unknown <BAL21613@BAL21613.balsam.asheville.cc.nc.us>
2015-04-23 09:51:27 -0400

    phase 6: added about page

--- web/app/controllers/AboutController.php
+++ web/app/controllers/AboutController.php
@@ -1,13 +1,17 @@
 <?php
+// index controller for About page
 class AboutController extends ControllerBase
 {
+	// initialize and set title 
     public function initialize()
     {
         $this->tag->setTitle('About');
         parent::initialize();
     }
+	
+	// action function required
 	public function indexAction()
 	{
 
-	}
+	} 
 }
